724512c2fabca5fc731195db2a8e19142d1fe93b
Jordano Giacomet Tomazoni <jgtomazoni@ucs.br>
2025-04-09 14:55:22 -0300

    WIP - Aula 29 completa;

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
 import { DefaultInput } from './components/DefaultInput';
+import { Cycles } from './components/Cycles';
 
 export function App() {
   return (
@@ -33,7 +34,9 @@ export function App() {
           <div className='formRow'>
             <p>Lorem ipsum dolor sit amet.</p>
           </div>
-          <div className='formRow'></div>
+          <div className='formRow'>
+            <Cycles />
+          </div>
           <div className='formRow'>
             <button>Enviar</button>
           </div>
