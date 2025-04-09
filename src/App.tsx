e398e8dbe08438c8d5aa613972f8dfbccfd49063
Jordano Giacomet Tomazoni <jgtomazoni@ucs.br>
2025-04-09 13:23:59 -0300

    WIP - Aula 23 completa;

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import './styles/theme.css';
 import './styles/global.css';
-import { Fragment } from 'react/jsx-runtime';
 
-import { Heading } from './components/Heading';
+import { Fragment } from 'react/jsx-runtime';
 import { Container } from './components/Container';
+import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
 
 export function App() {
   return (
     <Fragment>
       <Container>
-        <Heading>LOGO</Heading>
+        <Logo />
       </Container>
       <Container>
-        <Heading>MENU</Heading>
+        <Menu />
       </Container>
     </Fragment>
   );
